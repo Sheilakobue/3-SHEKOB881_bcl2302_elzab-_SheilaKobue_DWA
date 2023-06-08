a44deb507976808ad8f0913f5eac4b42a74dd2da
Sheila Kobue <maserole.mojela@gmail.com>
2023-06-08 10:08:13 +0200

    added new files

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA5_Challenge1/scripts.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA5_Challenge1/scripts.js
@@ -6,7 +6,7 @@
  * @returns {HTMLElement}
  */
 
-const getHtml = (label) => {
+/**const getHtml = (label) => {
     const node = document.querySelector(`[data-${label}]`)
     if (!(node instanceof HTMLElement)){
         throw new Error(
@@ -15,32 +15,37 @@ const getHtml = (label) => {
     }
     return node
 }
-
+*/
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault(); // prevents form from submission
-  const entries = new FormData(event.target);
-  const {dividend,divider}=Object.fromEntries(entries);
+    event.preventDefault(); // prevents form from submission behavior, which can cause page reload
+    const entries = new FormData(event.target);
+    const {dividend,divider}=Object.fromEntries(entries);
  
-  const isWholeNumber = Number.isInteger(result); // checks if results is a whole number
+    const isWholeNumber = Number.isInteger(result); // checks if results is a whole number
+
 try{
+  //checks if any input is missing
 if (divider === "" || dividend === ""){
     result.innerText = "Division not performed. Both values are required in inputs. Try again.";
-    
-} else if (divider <= 0 || dividend <= 0) {
-  result.innerText = "Division not performed. Invalid number provided. Try again.";
 
+  //checks if the input is less tha 0 or equals to 0
+} else if (divider <= 0 || dividend <= 0) {
+    result.innerText = "Division not performed. Invalid number provided. Try again.";
+
+  //checks if the input is not a number 
 }else if (isNaN(dividend) || isNaN(divider)) {
     
+  //if its not a number it logs an error msg to the console
     console.error("Invalid input. Expected numbers.");
     console.trace(); // Logs call stack
-    document.body.innerHTML = "Something critical went wrong. Please reload the page."; // Clear the screen
+    document.body.innerHTML = "Something critical went wrong. Please reload the page."; // Clear the screen & replace the contect of the document.body with a msg
 
 } else {
-  const resultForDivision = dividend / divider;
-  result.innerText = Math.floor(resultForDivision)
+    const resultForDivision = dividend / divider;
+    result.innerText = Math.floor(resultForDivision)
 }
 
 
