a2f13d1e94a043596ea6670bfab7923f9a1b08cc
Sheila Kobue <maserole.mojela@gmail.com>
2023-06-19 16:14:12 +0200

    deleted files

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA9/book-connect/index.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA9/book-connect/index.js
@@ -1,5 +1,68 @@
 const template = document.createElement("template");
-template.innerHTML=
+template.innerHTML=`
+<dialog class="overlay" data-list-active>
+      <div class="overlay__preview"><img class="overlay__blur" data-list-blur src=""/><img class="overlay__image" data-list-image src=""/></div>
+      <div class="overlay__content">
+        <h3 class="overlay__title" data-list-title></h3>
+        <div class="overlay__data" data-list-subtitle></div>
+        <p class="overlay__data overlay__data_secondary" data-list-description></p>
+      </div>
+
+      <div class="overlay__row">
+        <button class="overlay__button overlay__button_primary" data-list-close>Close</button>
+      </div>
+    </dialog>
+
+
+    <dialog class="overlay" data-search-overlay>
+      <div class="overlay__content">
+        <form class="overlay__form" data-search-form id="search">
+          <label class="overlay__field">
+            <div class="overlay__label">Title</div>
+            <input class="overlay__input" data-search-title name="title" placeholder="Any"></input>
+          </label>
+
+          <label class="overlay__field">
+            <div class="overlay__label">Genre</div>
+            <select class="overlay__input overlay__input_select" data-search-genres name="genre"></select>
+          </label>
+
+          <label class="overlay__field">
+            <div class="overlay__label">Author</div>
+            <select class="overlay__input overlay__input_select" data-search-authors name="author">
+            </select>
+          </label>
+        </form>
+
+        <div class="overlay__row">
+          <button class="overlay__button" data-search-cancel>Cancel</button>
+          <button class="overlay__button overlay__button_primary" type="submit" form="search">Search</button>
+        </div>
+      </div>
+    </dialog>
+
+    <dialog class="overlay" data-settings-overlay>
+      <div class="overlay__content">
+        <form class="overlay__form" data-settings-form id="settings">
+          <label class="overlay__field">
+            <div class="overlay__label">Theme</div>
+
+            <select class="overlay__input overlay__input_select" data-settings-theme name="theme">
+              <option value="day">Day</option>
+              <option value="night">Night</option>
+            </select>
+          </label>
+        </form>
+
+        <div class="overlay__row">
+          <button class="overlay__button" data-settings-cancel>Cancel</button>
+          <button class="overlay__button overlay__button_primary" type="submit" form="settings">Save</button>
+        </div>
+      </div>
+    </dialog>
+    
+    <div class="backdrop"></div>
+    `;
 
 
 class bookConnect extends HTMLElement{
@@ -9,3 +72,4 @@ class bookConnect extends HTMLElement{
         this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 }
+export default bookConnect;
---
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA9/index.js
@@ -0,0 +1,3 @@
+import bookConnect from "./book-connect/index.js";
+
+window.customElements.define('book-connect',bookConnect )
