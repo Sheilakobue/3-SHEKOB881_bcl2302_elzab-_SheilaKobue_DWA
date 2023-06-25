446c51ea7c93b04174a60eb381e6b5aae0f417cb
Sheila Kobue <maserole.mojela@gmail.com>
2023-06-25 23:28:33 +0200

    added the reset button

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA10/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA10/script.js
@@ -1,38 +1,25 @@
-const MAX_NUMBER = 15;
-const MIN_NUMBER = -5;
-const STEP_AMOUNT = 5;
-const number = document.querySelector('[data-key="number"]');
-const subtract = document.querySelector('[data-key="subtract"]');
-const add = document.querySelector('[data-key="add"]');
-const Reset = document.querySelector('[data-key="Reset"]');
-const confirmationMessage = document.getElementById('confirmationMessage');
-const subtractHandler = () => {
-  const newValue = parseInt(number.value) - 1;
-  number.value = newValue;
-  if (add.disabled === true) {
-    add.disabled = false;
+// script.js
+const counterDisplay = document.getElementById('counter');
+const addButton = document.getElementById('addButton');
+const subtractButton = document.getElementById('subtractButton');
+const resetButton = document.getElementById('resetButton');
+
+let counter = 0;
+
+addButton.addEventListener('click', () => {
+  counter++;
+  counterDisplay.textContent = counter;
+});
+
+subtractButton.addEventListener('click', () => {
+  if (counter > 0) {
+    counter--;
+    counterDisplay.textContent = counter;
   }
-  if (newValue <= MIN_NUMBER) {
-    subtract.disabled = true;
-  }
-};
-const addHandler = () => {
-  const newValue = parseInt(number.value) + 1;
-  number.value = newValue;
-  if (subtract.disabled === true) {
-    subtract.disabled = false;
-  }
-  if (newValue >= MAX_NUMBER) {
-    add.disabled = true;
-  }
-};
-const resetHandler = () => {
-  const initialValue = 0;
-  number.value = initialValue;
-  alert('Are sure you want to reset?');
-  subtract.disabled = false;
-  add.disabled = false;
-};
-subtract.addEventListener('click', subtractHandler);
-add.addEventListener('click', addHandler);
-Reset.addEventListener('click', resetHandler);
+});
+
+resetButton.addEventListener('click', () => {
+  counter = 0;
+  counterDisplay.textContent = counter;
+  // Show a confirmation message using Shoelace Toast component or any other suitable component
+});
