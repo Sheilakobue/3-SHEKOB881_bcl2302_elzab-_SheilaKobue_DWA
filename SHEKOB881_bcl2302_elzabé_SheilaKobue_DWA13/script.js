236edffebb8fac49621657558ebba15b8a37b4e8
Sheila Kobue <maserole.mojela@gmail.com>
2023-06-27 15:25:15 +0200

    edited file

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA13/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA13/script.js
@@ -1,8 +1,7 @@
- /**
+/**
  * An array of province names.
  * @type {string[]}
  */
-
 const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
 
 /**
@@ -11,62 +10,35 @@ const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', '
  */
 const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
 
-//Logging each name
-names.forEach(name => { //'forEach method is on the 'names' array to iterate(over and over) over each element
-  console.log(name);
-});
+console.log(
+  // Logging each name
+  names.forEach(name => {
+    console.log(name);
+  }),
 
-//Logging each name with matching province
-names.forEach((name, index) => {//'forEach' method is used to also  provide an index parameter
-  console.log(`${name} (${provinces[index]})`);//the name and the corresponding province are logged to the console in the format ${name} (${province}).
-});
+  // Logging each name with matching province
+  names.forEach((name, index) => {
+    console.log(`${name} (${provinces[index]})`);
+  }),
 
-//Mapping province names to uppercase
-/**
- * An array of uppercase province names.
- * @type {string[]}
- */
-const uppercasedProvinces = provinces.map(province => province.toUpperCase());// map method is used on the province array to create a new array
+  // Mapping province names to uppercase
+  provinces.map(province => province.toUpperCase()),
 
-//Creating an array with the length of each name
-/**
- * An array of name lengths.
- * @type {number[]}
- */
-const nameLengths = names.map(name => name.length);
+  // Creating an array with the length of each name
+  names.map(name => name.length),
 
+  // Sorting provinces alphabetically
+  provinces.sort(),
 
-//Sorting provinces alphabetically
-/**
- * An array of sorted provinces.
- * @type {string[]}
- */
-const sortedProvinces = provinces.sort();
+  // Filtering provinces without the word "Cape" and returning the count
+  provinces.filter(province => !province.includes('Cape')).length,
 
-//Filtering provinces without the word "Cape" and returning the count
-/**
- * An array of filtered provinces.
- * @type {string[]}
- */
-const filteredProvinces = provinces.filter(province => !province.includes('Cape'));
-console.log(filteredProvinces.length);
+  // Creating a boolean array indicating whether each name contains an 'S' character
+  names.map(name => name.includes('S')),
 
-/**
- * A boolean array indicating whether each name contains an 'S' character.
- * @type {boolean[]}
- */
-const hasSCharacter = names.map(name => name.includes('S'));
-
-
-//Creating an object indicating the province of each individual
-/**
- * An object indicating the province of each individual.
- * @type {Object.<string, string>}
- */
-const provinceObject = names.reduce((obj, name, index) => {
-  obj[name] = provinces[index];
-  return obj;
-}, {});
-
-console.log(uppercasedProvinces, nameLengths,sortedProvinces, hasSCharacter, provinceObject);
-
+  // Creating an object indicating the province of each individual
+  names.reduce((obj, name, index) => {
+    obj[name] = provinces[index];
+    return obj;
+  }, {})
+);
