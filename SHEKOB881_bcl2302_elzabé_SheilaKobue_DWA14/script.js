6e545aa2defc3889d0935907689d85e99554d56c
Sheila Kobue <maserole.mojela@gmail.com>
2023-06-28 16:05:17 +0200

    imported file

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA14/script.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA14/script.js
@@ -1,3 +1,5 @@
+import "./tc-app.js"
+
 // redux
 
 // Define constants
--- SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA14/tc-app.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA14/tc-app.js
@@ -1,1 +1,10 @@
-import{} from  
+import{ LitElement, html} from '../libs/lit-html.js'
+
+
+class App extends LitElement{
+    /**@ return {any} */
+    render(){
+        return html `<div>123</div>`;
+    }
+}
+customElements.define("tc-app", App);
