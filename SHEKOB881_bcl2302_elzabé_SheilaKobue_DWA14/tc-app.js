1b7396630424011df5bd3e530cc3c56131a1e771
Sheila Kobue <maserole.mojela@gmail.com>
2023-06-30 13:21:29 +0200

    added new files

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA14/tc-app.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA14/tc-app.js
@@ -1,30 +1,19 @@
-import{ LitElement, html} from '../libs/lit-html.js'
+ import{ LitElement, html} from '../libs/lit-html.js'
 
-class Adding extends LitElement{
-    static properties = {
-      open:{ type: "boolean", state: true}
-    }
+class tcApp extends LitElement{
+   // static properties = {
+     // open:{ type: "boolean", state: true}
+    
 
   constructor(){
     super(),
     this.open = false;
   }
     render(){
-        console.log(this.open);
-        
+                
         return html `
-        <div>
-          
-        <main class="counter">
-        <input class="counter_value" data-key="number" readonly value="0" />
-        <div class="counter_actions">
-            <sl-button data-key="subtract" class="counter_button counter_button_first">-</sl-button>
-            <sl-button data-key="add" class="counter_button">+</sl-button>
-        </div>
-        </main>
-
-        </div>
+        <p> hello</p>
         `;
     }
 }
-customElements.define("tc-app", App);
+customElements.define("tc-app", tcApp);
