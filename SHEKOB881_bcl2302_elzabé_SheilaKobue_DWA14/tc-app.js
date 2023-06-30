3ea9e31237635992b62488bed8ec877161b10ab3
Sheila Kobue <maserole.mojela@gmail.com>
2023-06-30 13:53:57 +0200

    created file

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA14/tc-app.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA14/tc-app.js
@@ -1,18 +1,29 @@
  import{ LitElement, html} from '../libs/lit-html.js'
 
 class tcApp extends LitElement{
-   // static properties = {
-     // open:{ type: "boolean", state: true}
-    
+    static properties = {
+      open:{ type: "boolean", state: true}
+    }
 
   constructor(){
     super(),
     this.open = false;
   }
     render(){
-                
+        console.log(this.open);
+        
         return html `
-        <p> hello</p>
+        <div>
+          
+        <main class="counter">
+        <input class="counter_value" data-key="number" readonly value="0" />
+        <div class="counter_actions">
+            <sl-button data-key="subtract" class="counter_button counter_button_first">-</sl-button>
+            <sl-button data-key="add" class="counter_button">+</sl-button>
+        </div>
+        </main>
+
+        </div>
         `;
     }
 }
