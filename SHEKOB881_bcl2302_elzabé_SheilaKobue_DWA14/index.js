b333d6a3d29058ac41312f29ad41b5d038261a63
Sheila Kobue <maserole.mojela@gmail.com>
2023-07-03 13:58:55 +0200

    edited file

--- SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA14/index.js
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA14/index.js
@@ -1,2 +1,2 @@
 import './style.css';
-import './tc-app.js'
+import './tally-app.js'
---
+++ SHEKOB881_bcl2302_elzabé_SheilaKobue_DWA14/tally-app.js
@@ -0,0 +1,215 @@
+import {LitElement, css, html} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+const MAX_NUMBER = 15;
+const MIN_NUMBER = -5;
+const STEP_AMOUNT = 5;
+
+export class TallyApp extends LitElement {
+  static styles = css`
+:host {
+    --color-green: #0a0d29;
+    --color-white: #85a1cf;
+    --color-dark-grey: #070731;
+    --color-medium-grey: hsl(240, 68%, 16%);
+    --color-light-grey: #9CA3AE;
+}
+
+* {
+    :box-sizing: border-box;
+}
+
+html {
+    :height: 100vh;
+}
+
+: body {
+    margin: 0;
+    background: var(--color-medium-grey);
+    color: var(--color-blue);
+    font-family: roboto, Arial, Helvetica, sans-serif;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+}
+
+/* header*/
+.header {
+    text-align: center;
+}
+
+.header_title {
+    font-size: 3rem;
+    font-weight: 900;
+    color: var(--color-light-grey);
+}
+
+/*control*/
+.controls {
+    background: rgb(48, 64, 169);
+}
+
+/*counter*/
+.counter {
+    background: var(--color-dark-grey);
+    }
+
+.counter_value {
+    width: 100%;
+    height: 15rem;
+    text-align: center;
+    font-size: 6rem;
+    font-weight: 900;
+    background: no-repeat;
+    color: var(--color-white);
+    border-width: 0;
+    border-bottom: 1px solid var(--color-light-grey);
+}
+
+.counter_actions {
+    display: flex;
+}
+
+.counter_button {
+    background: none;
+    width: 20%;
+    color: var(--color-white);
+    font-size: 3rem;
+    transition: transform 0. 3s;
+    transform: translateY(0);
+    left:30%
+}
+
+.counter_button:disabled {
+    opacity: 0.2
+}
+
+.counter_button:active {
+    background: var(--color-medium-grey);
+    transform: translateY(2%);
+}
+
+.counter_button_first {
+    border-right: 3px solid var(--color-light-grey);
+    
+}
+
+/*footer*/
+.footer {
+    background: var(--color-dark-grey);
+    color: var(--color-light-grey);
+    padding: 2rem;
+    font-size: 0, 8rem;
+    text-align: center;
+}
+
+.footer_link P {
+    color: var(--color-white);
+}
+  `;
+    static properties = {
+    counterValue: { type: Number },
+    counterState: { type: String },
+  };
+
+  constructor() {
+    super();
+    this.counterValue = 0;
+   
+  }
+
+  render() {
+    return html`
+      <header class="header">
+        <h1 class="header_title">
+          Tally counter
+        </h1>
+      </header>
+
+      <aside class="controls">
+        <label>
+          <span>Display</span>
+          <select>
+            <option>Single</option>
+            <option>Multiple</option>
+          </select>
+          <select>
+            <span>Counter</span>
+            <option>Counter 1</option>
+            <option>Counter 2</option>
+            <option>Counter 3</option>
+          </select>
+        </label>
+        <button @click=${this.openSettings}>Settings</button>
+        <sl-button data-key="Reset" class="Reset" @click=${this.resetCounter}>
+          Reset
+        </sl-button>
+      </aside>
+
+      <main class="counter">
+        <input
+          class="counter_value"
+          data-key="number"
+          readonly
+          .value=${this.counterValue}
+        />
+        <div class="counter_actions">
+          <sl-button
+            data-key="subtract"
+            class="counter_button counter_button_first"
+            @click=${this.subtractValue}
+          >
+            -
+          </sl-button>
+          <sl-button
+            data-key="add"
+            class="counter_button"
+            @click=${this.addValue}
+          >
+            +
+          </sl-button>
+        </div>
+      </main>
+
+      <footer class="footer">
+        inspired by
+        <a class="footer_link" href="https://tallycount.app/">Tally Counter</a>.
+        Note that this is merely a student practice project for learning JavaScript.
+      </footer>
+    `;
+  }
+
+  openSettings() {
+    // Implement the logic to open settings
+  }
+
+  resetCounter() {
+    this.counterValue = 0;
+    this.counterState = 'Normal';
+  }
+
+  subtractValue() {
+    if (this.counterValue > 0) {
+      this.counterValue--;
+    }
+
+    if (this.counterValue === 0) {
+      this.counterState = 'Minimum Reached';
+    } else if (this.counterValue < 10) {
+      this.counterState = 'Normal';
+    }
+  }
+
+  addValue() {
+    if (this.counterValue < 10) {
+      this.counterValue++;
+    }
+
+    if (this.counterValue === 10) {
+      this.counterState = 'Maximum Reached';
+    } else if (this.counterValue > 0) {
+      this.counterState = 'Normal';
+    }
+  }
+}
+
+customElements.define('tally-app', TallyApp);
